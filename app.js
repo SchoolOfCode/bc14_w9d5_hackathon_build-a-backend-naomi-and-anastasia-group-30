ca3d2490993b75194a3945f9993bd0fe677dcfdc
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-05-12 10:25:53 +0100

    Added routes to get all recipes and get a single recipe by id

--- app.js
+++ app.js
@@ -8,11 +8,36 @@ import {
   deleteRecipeByID,
 } from "./recipes.js";
 
+// This is the server
+// This file contains the endpoints that the client will hit
+// This means that the route handlers are in this file 
+// The route handlers are the functions that will be called when the client hits the endpoints
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("public"));
 app.use(express.json());
+
+// Create a GET /api/recipes endpoint that returns all recipes
+app.get("/api/recipes", (req, res) => { // This is the endpoint that the client will hit to get all recipes from the server
+  async function allRecipes() { // This is the function that will be called when the client hits the endpoint
+    const result = await getRecipes(); // The getRecipes helper function will be called and its result will be saved to a variable called result
+    res.send(result); // The result will be sent back to the client as a response
+  }
+  allRecipes(); // The allRecipes function will be called
+  // So when the client hits the endpoint, the getRecipes helper function will be called and its result will be sent back to the client
+});
+
+// Create a GET /api/recipes/:id endpoint that returns a single recipe given an id
+app.get("/api/recipes/:id", (req, res) => { // This is the endpoint that the client will hit
+  async function singleRecipe() { // This is the function that will be called when the client hits the endpoint to look for a single recipe
+    const result = await getRecipeByID(req.params.id); // The getRecipeByID helper function will be called and its result will be saved to a variable called result
+    // We need to pass the id as an argument to the getRecipeByID helper function, so we're using the request object to get the id from the client
+    res.send(result); // The result will be sent back to the client as a response
+  }
+  singleRecipe(); // The singleRecipe function will be called and return the recipe with the matching id to the client as a response
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
--- recipes.js
+++ recipes.js
@@ -3,11 +3,47 @@ import { v4 as uuidv4 } from "uuid";
 
 const fileName = "recipes.json";
 
+// All the functions in this file are helper functions
+// This means that they are functions that will be called by other functions in the app.js file
+// To make the app.js file easier to read and understand
+// The app.js file will call these helper functions when the client hits the endpoints
+
 // GET ALL RECIPES
-export async function getRecipes() {}
+// Read the recipes.json file and await its response
+// Save it to a variable called recipesJSON
+// Parse the recipesJSON into a JavaScript array of objects
+// Return the array of objects
+export async function getRecipes() {
+    const recipesJSON = await fs.readFile(fileName); // Read the recipes.json file and await its response
+    const recipes = JSON.parse(recipesJSON); // Parse the recipesJSON into a JavaScript array of objects
+    return recipes; // Return the array of objects
+}
+// If it's not working on Thunder Client, remember to check the port number in the URL, plus to have no s in http
 
 // GET A RECIPE BY ID
-export async function getRecipeByID(id) {}
+// Read the recipes.json file and await its response
+// Save it to a variable called recipesJSON
+// Parse the recipesJSON into a JavaScript array of objects
+// Loop through the array of objects
+// If the id of the current object matches the id passed in as an argument, return the current object
+export async function getRecipeByID(id) { // This function will be called when the client hits the endpoint, this means when the client requests a recipe by id
+    const recipesJSON = await fs.readFile(fileName); // Read the recipes.json file and await its response
+    const recipes = JSON.parse(recipesJSON); // Parse the recipesJSON into a JavaScript array of objects
+    let recipeObject = null; // Declare a variable called recipeObject and set it to null
+    for (let i = 0; i < recipes.length; i++) { // Loop through the array of objects
+        if (recipes[i].id === id) { // If the id of the current object matches the id passed in as an argument
+          recipeObject = recipes[i]; // Set the recipeObject to the current object
+          // That means if the id passed in is 1, the recipeObject will be the object with id 1
+          break; // The loop will stop when it finds the first match with the id we're looking for
+        }
+      }
+    // if (recipeObject === null) { // If the recipeObject is still null, return an error message
+    //     throw new Error("Recipe not found");
+    // }
+    return recipeObject; // Return the recipeObject with the matching id
+}
+// In this function, we're looking for a particular recipe by the id we're giving it
+// So we need to pass the id as an argument to the function when we call it via the request object in the app.js file
 
 // CREATE A RECIPE
 export async function createRecipe(newRecipe) {}
